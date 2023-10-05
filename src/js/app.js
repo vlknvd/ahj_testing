bfaffb1588135debfbb0f893537073d922a50e93
vlknvd <116744722+vlknvd@users.noreply.github.com>
2023-10-05 20:21:55 +0300

    Delete src directory

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import Widget from './Widget';
-import systemPayCard from './systemPayCard';
-import AddEventWidget from './AddEventWidget';
-
-const container = document.querySelector('.container');
-const widget = new Widget(container);
-widget.redrawWidget();
-const eventWidget = new AddEventWidget(widget, systemPayCard);
-eventWidget.addEvent();
